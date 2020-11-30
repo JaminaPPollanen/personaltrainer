2ebddaa02371834fe7d05dc990e80737affde1e5
Jamina Pöllänen <bgj428@myy.haaga-helia.fi>
2020-11-30 16:29:11 +0200

    delete and edit added, link is incorrect, must fix

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -2,10 +2,17 @@ import React, {useState, useEffect, useRef} from 'react';
 import {AgGridReact} from'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import Addcustomer from './AddCustomer';
+import EditCustomer from './EditCustomer';
+import  Button  from '@material-ui/core/Button';
+import  Snackbar from '@material-ui/core/Snackbar';
 
 function Customerlist() {
 
     const [customers, setCustomers] = useState([]);
+    const [msg, SetMsg] = useState('');
+    const [open, setOpen] = useState(false);
+
     const gridRef = useRef();
 
     const getCustomers = () => {
@@ -19,6 +26,44 @@ function Customerlist() {
         getCustomers();
     }, []);
 
+    const addCustomer = (newCustomer) => {
+        fetch('https://customerrest.herokuapp.com/api/customers', {
+            method: 'POST',
+            headers: {'Content-type' : 'application/json'},
+            body: JSON.stringify(newCustomer)
+        })
+        .then(_ => gridRef.current.refreshCells({rowNodes: getCustomers()}))
+        .catch(err => console.log(err))
+    }
+
+    const updateCustomer = (link, customer) => {
+        fetch(link, {
+            method: 'PUT',
+            headers: {'Content-type' : 'application/json'},
+            body: JSON.stringify(customer)
+        })
+        .then(_ => gridRef.current.refreshCells({rowNodes: getCustomers()}))
+        .then(_ => SetMsg('Car was updated succesfully'))
+        .then(_ => setOpen(true))
+        .catch(err => console.log(err))
+    }
+
+    const deleteCustomer = (link) => {
+        if (window.confirm('Are you sure?')){
+            fetch(link, {
+                method: 'DELETE'
+            })
+            .then(_ => gridRef.current.refreshCells({rowNodes: getCustomers()}))
+            .then(_ => SetMsg('Customer was deleted succesfully'))
+            .then(_ => setOpen(true))
+            .catch(err => console.log(err))
+            }
+    }
+
+    const closeSnackbar = () => {
+        setOpen(false);
+    }
+
     const columns = [
         {headerName: 'Firstname', field: 'firstname', sortable: true, filter: true},
         {headerName: 'Lastname', field: 'lastname', sortable: true, filter: true},
@@ -27,11 +72,27 @@ function Customerlist() {
         {headerName: 'City', field: 'city', sortable: true, filter: true}, 
         {headerName: 'Email', field: 'email', sortable: true, filter: true},
         {headerName: 'Phone', field: 'phone', sortable: true, filter: true},
+        {
+            headerName: '',
+            field: 'links.[0].href',
+            width: 90,
+            cellRendererFramework: params => <EditCustomer updateCustomer={updateCustomer} params={params}/>
+        },
+        {
+            headerName: '',
+            field: 'links.[0].href',
+            width: 90,
+            cellRendererFramework: params => 
+            <Button color="secondary" size="small" onClick={()=> deleteCustomer(params.value)}>
+                Delete
+            </Button>
+        }
     ]
 
     return(
         <div>
         <h1> Customers</h1>
+        <Addcustomer addCustomer={addCustomer}/>
         <div className="ag-theme-material" style={{height:'700px', width:'90%', margin:'auto'}}>
            <AgGridReact 
                 ref={gridRef}
@@ -45,6 +106,12 @@ function Customerlist() {
                 paginationPageSize={10}
                 >
             </AgGridReact>
+            <Snackbar
+            open={open}
+            autoHideDuration={3000}
+            onClose={closeSnackbar}
+            message={msg}
+            />
         </div>
         </div>
     );
